7b19a51dadf64588f63481fd6b7b8c03a17e3774
dnbrwn11 <dabrown@pcl.com>
2026-04-16 15:51:06 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Change 'burbank-cost-model' to your GitHub repo name
-  base: '/burbank-cost-model/',
+  base: '/burbank-library-1/',
   build: {
     outDir: 'dist',
     sourcemap: false,
